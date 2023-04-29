8d5559b23282b34622d08b4a666a60ae6e7888a9
Patric Vinicios <psdev7@gmail.com>
2023-04-29 14:17:55 -0300

    refactor: add unique ID and create create statis function

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,30 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
   slug: Slug
-  authorId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, "createdAt">,
+    id?: UniqueEntityID
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id
+    )
+    return question
+  }
+}
